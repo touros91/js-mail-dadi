6d063bbe240bd0184ef190aafb2f4cd7568833b1
touros91 <andreageraci91@gmail.com>
2021-07-30 17:08:10 +0200

    Aggiunto dello stile

--- mail/js/script.js
+++ mail/js/script.js
@@ -5,28 +5,21 @@
 
 
 // creo una lista di email 
+
 var listaEmail = ["marcorossi@gmail.com" , "chiara_baggio@gmail.com", "luigi90@libero.it", "davidebianchi@hotmail.com", "gabriella7@yahoo.com"];
 
 // chiedo all'utente di inserire la sua email 
+
+
 var email = prompt("inserisci email");
 
 // controllo che sia nella lista di chi può accedere
-
 for (var i = 0; i < listaEmail.length; i++) {
 
     if (email == listaEmail[i]) {
-        alert("Puoi accedere!");
+        document.getElementById("auth").innerHTML = "Il tuo indirizzo email è registrato. Puoi accedere!"
         i = listaEmail.length;
     } else if (i == (listaEmail.length - 1)) {
-        alert("Non puoi accedere!");
+        document.getElementById("not-auth").innerHTML = "Il tuo indirizzo email non è registrato. Non sei autorizzato ad accedere!"
     }
 }
-
-
-
-// if (email=listaEmail) {
-//     alert("autorizzato");
-// } else {
-//     alert("non autorizzato");
-// }
-
