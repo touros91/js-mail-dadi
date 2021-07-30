7995a2e385da8d7427c5cbcae5878cf824a03ec0
touros91 <andreageraci91@gmail.com>
2021-07-30 17:41:28 +0200

    Aggiunto JS per esercizio Dadi

---
+++ dadi/js/script.js
@@ -0,0 +1,25 @@
+// genero un numero casuale tra 1 e 6 per il giocatore  
+
+var giocatore = Math.floor(Math.random() * 6) + 1;
+
+// genero un numero casuale tra 1 e 6 per il computer 
+
+var computer = Math.floor(Math.random() * 6) + 1;
+
+// Stabilisco il vincitore, in base a chi fa il punteggio più alto 
+
+if (giocatore > computer) {
+    document.getElementById("giocatore").innerHTML += giocatore + " HAI VINTO!";
+    document.getElementById("giocatore").style.color = "green";
+    document.getElementById("computer").innerHTML += computer;
+    document.getElementById("computer").style.color = "red";
+} else if (computer > giocatore) {
+    document.getElementById("computer").innerHTML += computer;
+    document.getElementById("giocatore").innerHTML += giocatore + " Hai perso! Oh nooo il computer ha vinto! Ritenta sarai più fortunato";
+    document.getElementById("giocatore").style.color = "red";  
+} else {
+    document.getElementById("giocatore").innerHTML += giocatore;
+    document.getElementById("computer").innerHTML += computer;
+    document.getElementById("pareggio").innerHTML += "Pareggio! Ripetere il lancio!";
+    // alert("Pareggio! Ripetere il lancio!");
+}
